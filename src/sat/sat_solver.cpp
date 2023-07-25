21f83edb067f17c9eaf196456a4e555db1436a80
hgvk94 <hgvk94@gmail.com>
2023-07-25 18:19:46 -0400

    in sms, decide can return null_var

--- src/sat/sat_solver.cpp
+++ src/sat/sat_solver.cpp
@@ -1660,6 +1660,7 @@ namespace sat {
         if (m_ext && m_ext->decide(next, phase)) {
             if (used_queue)
                 m_case_split_queue.unassign_var_eh(next_lit.var());
+            if (next == null_bool_var) return false;
             next_lit = literal(next, false);
         }
         if (inconsistent()) return true;
